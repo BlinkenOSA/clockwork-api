c288a61df3c0f0236938fc9992a2b54fd857c25f
JoshBone <jozsef.bone@gmail.com>
2019-05-30 15:16:00 +0200

    feat(Settings): Fix settings files.

--- clockwork_api/settings.py
+++ clockwork_api/settings.py
@@ -2,7 +2,6 @@ from split_settings.tools import optional, include
 
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 include(
