14a221046c4939cad39d387bfaa4d65616cb6cc2
JoshBone <jozsef.bone@gmail.com>
2019-05-31 11:37:35 +0200

    fix(Authority): Add serializers for select.

--- authority/serializers.py
+++ authority/serializers.py
@@ -1,8 +1,26 @@
 from rest_framework import serializers
-from authority.models import Country
+from authority.models import Country, Language
 
 
 class CountrySerializer(serializers.ModelSerializer):
     class Meta:
         model = Country
         fields = '__all__'
+
+
+class CountrySelectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = ('id', 'country')
+
+
+class LanguageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Language
+        fields = '__all__'
+
+
+class LanguageSelectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Language
+        fields = ('id', 'language')
