19e2054be0373f11802abe0e2dad18615596c590
JoshBone <jozsef.bone@gmail.com>
2019-07-10 17:33:54 +0200

    feat(apps): Add drf-yasg, djoser and corsheaders to the applications.

--- clockwork_api/settings_components/application.py
+++ clockwork_api/settings_components/application.py
@@ -20,7 +20,9 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'django_filters',
-    'corsheaders'
+    'corsheaders',
+    'djoser',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
