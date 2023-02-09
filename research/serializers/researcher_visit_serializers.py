b3fdc58fb64e11e82a4cf410f772dd5312ac7f59
Josh <jozsef.bone@gmail.com>
2023-02-09 09:34:14 +0100

    Update researcher_visit_serializers.py

--- research/serializers/researcher_visit_serializers.py
+++ research/serializers/researcher_visit_serializers.py
@@ -5,6 +5,8 @@ from research.models import ResearcherVisit
 
 class ResearcherVisitListSerializer(serializers.ModelSerializer):
     researcher = serializers.SlugRelatedField(slug_field='name', read_only=True)
+    check_in = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
+    check_out = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
 
     class Meta:
         model = ResearcherVisit
