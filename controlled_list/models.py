b122de5ccd9488bf2647d450126885cd10f2e96e
Josh <jozsef.bone@gmail.com>
2023-03-22 15:28:23 +0100

    Update models with str methods.

--- controlled_list/models.py
+++ controlled_list/models.py
@@ -7,6 +7,9 @@ class AccessRight(models.Model, DetectProtectedMixin):
     id = models.AutoField(primary_key=True)
     statement = models.CharField(unique=True, max_length=200)
 
+    def __str__(self):
+        return self.statement
+
     class Meta:
         db_table = 'controlled_rights_access'
         ordering = ['statement']
@@ -16,6 +19,9 @@ class ArchivalUnitTheme(models.Model, DetectProtectedMixin):
     id = models.AutoField(primary_key=True)
     theme = models.CharField(unique=True, max_length=200)
 
+    def __str__(self):
+        return self.theme
+
     class Meta:
         db_table = 'controlled_archival_unit_themes'
         ordering = ['theme']
@@ -24,6 +30,9 @@ class ArchivalUnitTheme(models.Model, DetectProtectedMixin):
 class Building(models.Model, DetectProtectedMixin):
     id = models.AutoField(primary_key=True)
     building = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.building
 
     class Meta:
         db_table = 'controlled_building'
@@ -40,6 +49,9 @@ class CarrierType(models.Model, DetectProtectedMixin):
     old_id = models.IntegerField(blank=True, null=True)
     jasper_file = models.CharField(max_length=100, blank=True, null=True)
 
+    def __str__(self):
+        return self.type
+
     class Meta:
         db_table = 'controlled_carrier_types'
         ordering = ['type']
@@ -55,6 +67,9 @@ class CorporationRole(models.Model, DetectProtectedMixin):
     user_updated = models.CharField(max_length=100, blank=True)
     date_updated = models.DateTimeField(blank=True, null=True, auto_now=True)
 
+    def __str__(self):
+        return self.role
+
     class Meta:
         db_table = 'controlled_corporation_roles'
         ordering = ['role']
@@ -64,6 +79,9 @@ class DateType(models.Model, DetectProtectedMixin):
     id = models.AutoField(primary_key=True)
     type = models.CharField(unique=True, max_length=100)
 
+    def __str__(self):
+        return self.type
+
     class Meta:
         db_table = 'controlled_date_types'
         ordering = ['type']
@@ -72,6 +90,9 @@ class DateType(models.Model, DetectProtectedMixin):
 class ExtentUnit(models.Model, DetectProtectedMixin):
     id = models.AutoField(primary_key=True)
     unit = models.CharField(unique=True, max_length=100)
+
+    def __str__(self):
+        return self.unit
 
     class Meta:
         db_table = 'controlled_extent_units'
@@ -88,6 +109,9 @@ class GeoRole(models.Model, DetectProtectedMixin):
     user_updated = models.CharField(max_length=100, blank=True)
     date_updated = models.DateTimeField(blank=True, null=True, auto_now=True)
 
+    def __str__(self):
+        return self.role
+
     class Meta:
         db_table = 'controlled_geo_roles'
         ordering = ['role']
@@ -103,6 +127,9 @@ class Keyword(models.Model, DetectProtectedMixin):
     user_updated = models.CharField(max_length=100, blank=True)
     date_updated = models.DateTimeField(blank=True, null=True, auto_now=True)
 
+    def __str__(self):
+        return self.keyword
+
     class Meta:
         db_table = 'controlled_keywords'
         ordering = ['keyword']
@@ -111,6 +138,9 @@ class Keyword(models.Model, DetectProtectedMixin):
 class LanguageUsage(models.Model, DetectProtectedMixin):
     id = models.AutoField(primary_key=True)
     usage = models.CharField(unique=True, max_length=100)
+
+    def __str__(self):
+        return self.usage
 
     class Meta:
         db_table = 'controlled_language_usages'
@@ -130,6 +160,9 @@ class Nationality(models.Model, DetectProtectedMixin):
     id = models.AutoField(primary_key=True)
     nationality = models.CharField(unique=True, max_length=150)
 
+    def __str__(self):
+        return self.nationality
+
     class Meta:
         db_table = 'controlled_nationalities'
         ordering = ['nationality']
@@ -145,6 +178,9 @@ class PersonRole(models.Model, DetectProtectedMixin):
     user_updated = models.CharField(max_length=100, blank=True)
     date_updated = models.DateTimeField(blank=True, null=True, auto_now=True)
 
+    def __str__(self):
+        return self.role
+
     class Meta:
         db_table = 'controlled_person_roles'
         ordering = ['role']
@@ -153,6 +189,9 @@ class PersonRole(models.Model, DetectProtectedMixin):
 class PrimaryType(models.Model, DetectProtectedMixin):
     id = models.AutoField(primary_key=True)
     type = models.CharField(unique=True, max_length=20)
+
+    def __str__(self):
+        return self.type
 
     class Meta:
         db_table = 'controlled_primary_types'
@@ -163,6 +202,9 @@ class ReproductionRight(models.Model, DetectProtectedMixin):
     id = models.AutoField(primary_key=True)
     statement = models.CharField(unique=True, max_length=200)
 
+    def __str__(self):
+        return self.statement
+
     class Meta:
         db_table = 'controlled_rights_reproduction'
         ordering = ['statement']
@@ -171,6 +213,9 @@ class ReproductionRight(models.Model, DetectProtectedMixin):
 class RightsRestrictionReason(models.Model, DetectProtectedMixin):
     id = models.AutoField(primary_key=True)
     reason = models.CharField(unique=True, max_length=200)
+
+    def __str__(self):
+        return self.reason
 
     class Meta:
         db_table = 'controlled_rights_restriction_reason'
