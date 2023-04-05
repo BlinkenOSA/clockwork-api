db1e3504181002ebba87b171700003bd1dbf8728
Josh <jozsef.bone@gmail.com>
2023-04-05 12:38:52 +0200

    Add signal to ArchivalUnit

--- archival_unit/apps.py
+++ archival_unit/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class ArchivalUnitConfig(AppConfig):
     name = 'archival_unit'
+
+    def ready(self):
+        from . import signals
---
+++ archival_unit/signals.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from archival_unit.models import ArchivalUnit
+from isad.tasks import index_catalog_isad_record, index_catalog_isad_record_remove
+
+
+@receiver(post_save, sender=ArchivalUnit)
+def update_isad_when_archival_unit_saved(sender, instance, **kwargs):
+    if instance.isad:
+        if instance.isad.published:
+            index_catalog_isad_record.delay(isad_id=instance.isad.id)
+        else:
+            index_catalog_isad_record_remove.delay(isad_id=instance.isad.id)
