93bb1be1caf2b812a4f3711a91cd207ab13e0643
JoshBone <jozsef.bone@gmail.com>
2023-05-30 15:03:10 +0200

    WikiData views.

---
+++ authority/views/wikidata_views.py
@@ -0,0 +1,46 @@
+import json
+
+import requests
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class WikidataMixin(object):
+    def get_wikidata_links(self, query):
+        if len(query) == 0:
+            return []
+        else:
+            session = requests.Session()
+            session.trust_env = False
+
+            r = session.get(
+                'https://www.wikidata.org/w/api.php?action=wbsearchentities&format=json&search=%s&language=en' % query
+            )
+            if r.status_code == 200:
+                session.close()
+                return self.assemble_data_stream(json.loads(r.text))
+            else:
+                session.close()
+                return []
+
+    @staticmethod
+    def assemble_data_stream(json_data):
+        data = []
+
+        if 'search' in json_data:
+            for record in json_data['search']:
+                id = record['id']
+                rec = {
+                    'wikidata_id': id,
+                    'wikidata_url': record['concepturi'],
+                    'name': "%s (%s)" % (record['label'], record['description'])
+                }
+                data.append(rec)
+        return data
+
+
+class WikidataList(WikidataMixin, APIView):
+    def get(self, request, *args, **kwargs):
+        query = request.query_params.get('query', '')
+        data = self.get_wikidata_links(query)
+        return Response(data)
