e9589f432b0fe2235cef7677e949e3bcdc2175fb
Josh <jozsef.bone@gmail.com>
2023-11-17 08:57:34 +0100

    Indexer task updates

--- isad/tasks.py
+++ isad/tasks.py
@@ -2,15 +2,20 @@
 from celery import shared_task
 
 from isad.indexers.isad_catalog_indexer import ISADCatalogIndexer
+from isad.indexers.isad_new_catalog_indexer import ISADNewCatalogIndexer
 
 
 @shared_task
 def index_catalog_isad_record(isad_id):
     indexer = ISADCatalogIndexer(isad_id)
     indexer.index()
+    indexer_new = ISADNewCatalogIndexer(isad_id)
+    indexer_new.index_with_requests()
 
 
 @shared_task
 def index_catalog_isad_record_remove(isad_id):
     indexer = ISADCatalogIndexer(isad_id)
     indexer.delete()
+    indexer_new = ISADNewCatalogIndexer(isad_id)
+    indexer_new.delete()
