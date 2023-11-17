2b4c2a764d7f1947ae02339fcc4be1e060348c70
JoshBone <jozsef.bone@gmail.com>
2023-11-17 15:00:58 +0100

    Add new indexers to tasks.

--- finding_aids/tasks.py
+++ finding_aids/tasks.py
@@ -1,15 +1,20 @@
 # Create your tasks here
 from celery import shared_task
 from finding_aids.indexers.finding_aids_catalog_indexer import FindingAidsCatalogIndexer
+from finding_aids.indexers.finding_aids_new_catalog_indexer import FindingAidsNewCatalogIndexer
 
 
 @shared_task
 def index_catalog_finding_aids_entity(finding_aids_entity_id):
     indexer = FindingAidsCatalogIndexer(finding_aids_entity_id)
     indexer.index()
+    indexer = FindingAidsNewCatalogIndexer(finding_aids_entity_id)
+    indexer.index_with_requests()
 
 
 @shared_task
 def index_catalog_finding_aids_entity_remove(finding_aids_entity_id):
     indexer = FindingAidsCatalogIndexer(finding_aids_entity_id)
+    indexer.delete()
+    indexer = FindingAidsNewCatalogIndexer(finding_aids_entity_id)
     indexer.delete()
