b0c2db569df9226fc332b6e9553c7cb3ca47d940
Josh <jozsef.bone@gmail.com>
2026-03-06 12:05:22 +0100

    Archival Unit tests

--- archival_unit/tests/test_serializers.py
+++ archival_unit/tests/test_serializers.py
@@ -1,5 +1,12 @@
 from django.test import TestCase
-from archival_unit.serializers import ArchivalUnitWriteSerializer
+
+from archival_unit.models import ArchivalUnit
+from archival_unit.serializers import (
+    ArchivalUnitWriteSerializer,
+    ArchivalUnitPreCreateSerializer,
+    ArchivalUnitReadSerializer,
+    ArchivalUnitSelectSerializer,
+)
 
 
 class ArchivalUnitSerializerTest(TestCase):
@@ -27,3 +34,99 @@ class ArchivalUnitSerializerTest(TestCase):
         serializer.is_valid()
         self.assertFalse(serializer.is_valid())
         self.assertEqual(serializer.errors['level'][0], "Level should be either: 'Fonds', 'Subfonds', 'Series'")
+
+    def test_precreate_serializer_for_fonds(self):
+        fonds = ArchivalUnit.objects.create(
+            fonds=100,
+            level='F',
+            title='Fonds Title',
+            acronym='FT'
+        )
+        data = ArchivalUnitPreCreateSerializer(fonds).data
+        self.assertEqual(data['parent'], fonds.id)
+        self.assertEqual(data['fonds_title'], 'Fonds Title')
+        self.assertEqual(data['fonds_acronym'], 'FT')
+        self.assertIsNone(data['subfonds_title'])
+        self.assertIsNone(data['subfonds_acronym'])
+        self.assertEqual(data['level'], 'SF')
+
+    def test_precreate_serializer_for_subfonds(self):
+        fonds = ArchivalUnit.objects.create(
+            fonds=100,
+            level='F',
+            title='Fonds Title',
+            acronym='FT'
+        )
+        subfonds = ArchivalUnit.objects.create(
+            fonds=100,
+            subfonds=1,
+            level='SF',
+            title='Subfonds Title',
+            acronym='SFT',
+            parent=fonds
+        )
+        data = ArchivalUnitPreCreateSerializer(subfonds).data
+        self.assertEqual(data['fonds_title'], 'Fonds Title')
+        self.assertEqual(data['fonds_acronym'], 'FT')
+        self.assertEqual(data['subfonds_title'], 'Subfonds Title')
+        self.assertEqual(data['subfonds_acronym'], 'SFT')
+        self.assertEqual(data['level'], 'S')
+
+    def test_read_serializer_titles_for_series(self):
+        fonds = ArchivalUnit.objects.create(
+            fonds=100,
+            level='F',
+            title='Fonds Title',
+            acronym='FT'
+        )
+        subfonds = ArchivalUnit.objects.create(
+            fonds=100,
+            subfonds=1,
+            level='SF',
+            title='Subfonds Title',
+            acronym='SFT',
+            parent=fonds
+        )
+        series = ArchivalUnit.objects.create(
+            fonds=100,
+            subfonds=1,
+            series=1,
+            level='S',
+            title='Series Title',
+            parent=subfonds
+        )
+        data = ArchivalUnitReadSerializer(series).data
+        self.assertEqual(data['fonds_title'], 'Fonds Title')
+        self.assertEqual(data['fonds_acronym'], 'FT')
+        self.assertEqual(data['subfonds_title'], 'Subfonds Title')
+        self.assertEqual(data['subfonds_acronym'], 'SFT')
+
+    def test_select_serializer_counts(self):
+        fonds = ArchivalUnit.objects.create(
+            fonds=200,
+            level='F',
+            title='Fonds Title'
+        )
+        subfonds = ArchivalUnit.objects.create(
+            fonds=200,
+            subfonds=1,
+            level='SF',
+            title='Subfonds Title',
+            parent=fonds
+        )
+        series = ArchivalUnit.objects.create(
+            fonds=200,
+            subfonds=1,
+            series=1,
+            level='S',
+            title='Series Title',
+            parent=subfonds
+        )
+
+        fonds_data = ArchivalUnitSelectSerializer(fonds).data
+        series_data = ArchivalUnitSelectSerializer(series).data
+
+        self.assertIsNone(fonds_data['container_count'])
+        self.assertIsNone(fonds_data['folder_count'])
+        self.assertEqual(series_data['container_count'], 0)
+        self.assertEqual(series_data['folder_count'], 0)
--- archival_unit/tests/test_views.py
+++ archival_unit/tests/test_views.py
@@ -59,3 +59,57 @@ class ArchivalUnitViewTest(TestViewsBaseClass):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['title'], 'Updated Title')
         self.assertEqual(response.data['user_updated'], self.user.username)
+
+    def test_precreate_endpoint_for_fonds(self):
+        response = self.client.get(reverse('archival_unit-v1:archival_unit-pre-create',
+                                           kwargs={'pk': self.fonds.pk}))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['parent'], self.fonds.id)
+        self.assertEqual(response.data['level'], 'SF')
+
+    def test_select_by_parent_respects_allowed_units(self):
+        subfonds_allowed = ArchivalUnit.objects.create(
+            fonds=206,
+            subfonds=1,
+            level='SF',
+            title='Allowed Subfonds',
+            parent=self.fonds
+        )
+        subfonds_blocked = ArchivalUnit.objects.create(
+            fonds=206,
+            subfonds=2,
+            level='SF',
+            title='Blocked Subfonds',
+            parent=self.fonds
+        )
+        series_allowed = ArchivalUnit.objects.create(
+            fonds=206,
+            subfonds=1,
+            series=1,
+            level='S',
+            title='Allowed Series',
+            parent=subfonds_allowed
+        )
+        ArchivalUnit.objects.create(
+            fonds=206,
+            subfonds=2,
+            series=1,
+            level='S',
+            title='Blocked Series',
+            parent=subfonds_blocked
+        )
+        self.user_profile.allowed_archival_units.add(series_allowed)
+
+        response = self.client.get(
+            reverse('archival_unit-v1:archival_unit-select-by-parent-list', kwargs={'parent_id': self.fonds.id})
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        returned_ids = {item['id'] for item in response.data}
+        self.assertEqual(returned_ids, {subfonds_allowed.id})
+
+        response = self.client.get(
+            reverse('archival_unit-v1:archival_unit-select-by-parent-list', kwargs={'parent_id': subfonds_allowed.id})
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        returned_ids = {item['id'] for item in response.data}
+        self.assertEqual(returned_ids, {series_allowed.id})
