648313d1d19a6004ed19781800c8ace31fe1fa04
Josh <jozsef.bone@gmail.com>
2026-04-15 12:52:25 +0200

    Update populate_container_level_digital_versions.py

--- digitization/management/commands/populate_container_level_digital_versions.py
+++ digitization/management/commands/populate_container_level_digital_versions.py
@@ -8,41 +8,50 @@ from finding_aids.models import FindingAidsEntity
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        self.create_access_copies()
-        self.create_masters()
+        containers = list(self.get_target_containers())
+        self.create_access_copies(containers)
+        self.create_masters(containers)
 
-    def create_masters(self):
-        for container in Container.objects.filter(digital_version_exists=True):
-            if container.barcode:
-                identifier = container.barcode
+    def get_target_containers(self):
+        return Container.objects.filter(
+            digital_version_exists=True,
+            digital_versions__isnull=True
+        ).exclude(
+            barcode__isnull=True
+        ).exclude(
+            barcode=''
+        )
 
-                # Create entry for access files
-                digital_version, created = DigitalVersion.objects.get_or_create(
-                    container=container,
-                    identifier=identifier,
-                    level='M'
-                )
-                digital_version.filename = "%s.%s" % (identifier, "avi")
-                digital_version.creation_date = container.digital_version_creation_date
-                digital_version.available_online = False
-                digital_version.available_research_cloud = False
-                digital_version.save()
+    def create_masters(self, containers):
+        for container in containers:
+            identifier = container.barcode
 
-    def create_access_copies(self):
-        for container in Container.objects.filter(digital_version_exists=True):
-            if container.barcode:
-                identifier = container.barcode
+            # Create entry for access files
+            digital_version, created = DigitalVersion.objects.get_or_create(
+                container=container,
+                identifier=identifier,
+                level='M'
+            )
+            digital_version.filename = "%s.%s" % (identifier, "avi")
+            digital_version.creation_date = container.digital_version_creation_date
+            digital_version.available_online = False
+            digital_version.available_research_cloud = False
+            digital_version.save()
 
-                # Create entry for access files
-                digital_version, created = DigitalVersion.objects.get_or_create(
-                    container=container,
-                    identifier=identifier,
-                    level='A'
-                )
-                digital_version.filename = "%s.%s" % (identifier, "mp4")
-                digital_version.creation_date = container.digital_version_creation_date
-                digital_version.technical_metadata = container.digital_version_technical_metadata
-                digital_version.available_online = container.digital_version_online
-                digital_version.available_research_cloud = container.digital_version_research_cloud
-                digital_version.research_cloud_path = container.digital_version_research_cloud_path
-                digital_version.save()
+    def create_access_copies(self, containers):
+        for container in containers:
+            identifier = container.barcode
+
+            # Create entry for access files
+            digital_version, created = DigitalVersion.objects.get_or_create(
+                container=container,
+                identifier=identifier,
+                level='A'
+            )
+            digital_version.filename = "%s.%s" % (identifier, "mp4")
+            digital_version.creation_date = container.digital_version_creation_date
+            digital_version.technical_metadata = container.digital_version_technical_metadata
+            digital_version.available_online = container.digital_version_online
+            digital_version.available_research_cloud = container.digital_version_research_cloud
+            digital_version.research_cloud_path = container.digital_version_research_cloud_path
+            digital_version.save()
