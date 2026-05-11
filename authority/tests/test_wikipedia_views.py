26a24503246d34d398ab900925cf6b30ca5415d5
Josh <jozsef.bone@gmail.com>
2026-05-11 15:20:51 +0200

    Update test_wikipedia_views.py

--- authority/tests/test_wikipedia_views.py
+++ authority/tests/test_wikipedia_views.py
@@ -1,5 +1,7 @@
 from rest_framework import status
 from rest_framework.reverse import reverse
+from unittest.mock import patch
+
 from clockwork_api.tests.test_views_base_class import TestViewsBaseClass
 
 
@@ -9,8 +11,19 @@ class WikipediaTest(TestViewsBaseClass):
     def setUp(self):
         super().setUp()
 
-    def test_get_query(self):
+    @patch('authority.views.wikipedia_views.wikipedia.search', return_value=['Vladimir Lenin', 'Lenin'])
+    @patch('authority.views.wikipedia_views.wikipedia.set_lang')
+    def test_get_query(self, mock_set_lang, mock_search):
         response = self.client.get(reverse('authority-v1:wikipedia-list'), {'query': 'Lenin'})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        has_result = len(list(filter(lambda r: r['url'] == "http://en.wikipedia.org/wiki/Vladimir Lenin", response.data))) == 1
-        self.assertTrue(has_result)
+        self.assertEqual(len(response.data), 24)
+        self.assertIn(
+            {'name': 'Vladimir Lenin', 'url': 'http://en.wikipedia.org/wiki/Vladimir Lenin'},
+            response.data
+        )
+        self.assertIn(
+            {'name': 'Vladimir Lenin', 'url': 'http://hu.wikipedia.org/wiki/Vladimir Lenin'},
+            response.data
+        )
+        self.assertEqual(mock_set_lang.call_count, 12)
+        self.assertEqual(mock_search.call_count, 12)
