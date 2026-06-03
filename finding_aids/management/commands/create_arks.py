b70fdb22ce24acf0f65eb3938c7bab6d20a4476b
Josh <jozsef.bone@gmail.com>
2026-06-03 14:51:24 +0200

    Update create_arks.py

--- finding_aids/management/commands/create_arks.py
+++ finding_aids/management/commands/create_arks.py
@@ -1,3 +1,5 @@
+import time
+
 from django.core.management import BaseCommand
 from django.db.models import Q
 
@@ -23,15 +25,29 @@ class Command(BaseCommand):
             help="Optional list of primary keys to process.",
         )
         parser.add_argument(
+            "--fonds",
+            nargs="+",
+            type=int,
+            help="Optional list of fonds ids to process via archival_unit.fonds.",
+        )
+        parser.add_argument(
             "--dry-run",
             action="store_true",
             help="Show which records would be processed without minting ARKs.",
+        )
+        parser.add_argument(
+            "--sleep-ms",
+            type=int,
+            default=250,
+            help="Milliseconds to wait between mint requests. Default: 250.",
         )
 
     def handle(self, *args, **options):
         model_name = options["model"]
         ids = options.get("ids")
+        fonds = options.get("fonds")
         dry_run = options["dry_run"]
+        sleep_ms = max(options["sleep_ms"], 0)
 
         total_processed = 0
         total_created = 0
@@ -39,9 +55,11 @@ class Command(BaseCommand):
         if model_name in ("all", "finding_aids"):
             processed, created = self._handle_queryset(
                 label="finding_aids",
-                queryset=FindingAidsEntity.objects.filter(published=True).filter(Q(ark__isnull=True) | Q(ark="")),
+                queryset=FindingAidsEntity.objects.filter(published=True, is_template=False).filter(Q(ark__isnull=True) | Q(ark="")),
                 ids=ids,
+                fonds=fonds,
                 dry_run=dry_run,
+                sleep_ms=sleep_ms,
             )
             total_processed += processed
             total_created += created
@@ -51,7 +69,9 @@ class Command(BaseCommand):
                 label="isad",
                 queryset=Isad.objects.filter(published=True).filter(Q(ark__isnull=True) | Q(ark="")),
                 ids=ids,
+                fonds=fonds,
                 dry_run=dry_run,
+                sleep_ms=sleep_ms,
             )
             total_processed += processed
             total_created += created
@@ -62,9 +82,11 @@ class Command(BaseCommand):
             )
         )
 
-    def _handle_queryset(self, label, queryset, ids=None, dry_run=False):
+    def _handle_queryset(self, label, queryset, ids=None, fonds=None, dry_run=False, sleep_ms=0):
         if ids:
             queryset = queryset.filter(id__in=ids)
+        if fonds:
+            queryset = queryset.filter(archival_unit__fonds__in=fonds)
 
         processed = 0
         created = 0
@@ -81,9 +103,13 @@ class Command(BaseCommand):
             if ark:
                 created += 1
                 self.stdout.write("[%s] minted %s for id=%s (%s)" % (label, ark, instance.id, identifier))
+                if sleep_ms:
+                    time.sleep(sleep_ms / 1000)
             else:
                 self.stdout.write(
                     self.style.WARNING("[%s] failed to mint ARK for id=%s (%s)" % (label, instance.id, identifier))
                 )
+                if sleep_ms:
+                    time.sleep(sleep_ms / 1000)
 
         return processed, created
